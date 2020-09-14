56c2f4054de83e74fbe0f4b4258bd8fe2d833d58
Hayden Kerr <10897906@my.uvu.edu>
2020-09-14 17:16:58 -0600

    Created FollowPlayer script that follows vehicle as it moves and turning functionality

--- DGM_1610_Project/Assets/Scripts/PlayerController.cs
+++ DGM_1610_Project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed = 10f;
+    public float horizontalInput;
+    public float verticalInput; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
         // Will move the vehicle forward
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }

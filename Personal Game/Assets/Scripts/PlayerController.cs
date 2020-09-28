253f036755451c8d61fed8539fd247276ef30f1a
Hayden Kerr <10897906@my.uvu.edu>
2020-09-28 14:11:35 -0600

    Created personal project
    Added player and obstacle primitive shapes, also created scripts folder and PlayerController script

---
+++ Personal Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float hInput;
+    public float rotationSpeed = 25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gives player side to side movement
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+        
+    }
+}

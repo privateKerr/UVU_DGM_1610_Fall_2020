4a3d76f88b18f26c8ebf88a1472133c40c334399
Hayden Kerr <10897906@my.uvu.edu>
2020-10-03 17:59:21 -0600

    Created move forward script, added z axis boundary to constrict player movement

--- Personal Game/Assets/Scripts/PlayerController.cs
+++ Personal Game/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
     public float hInput;
-    public float rotationSpeed = 25;
+    public float vInput;
+    public float xRange = 8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,30 @@ public class PlayerController : MonoBehaviour
         // Gives player side to side movement
         hInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
-        
+        // Gives player forward movement
+        vInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * vInput * Time.deltaTime * speed);
+
+        // Constricts player movement on x axis
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        // Constricts player movement on z axis
+        if(transform.position.z < -12)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -12);
+        }
+        if (transform.position.z > -6)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -6);
+        }
+
     }
 }

bf9fa80e605ef68e62687272ad47205bf2d3b568
Hayden Kerr <10897906@my.uvu.edu>
2020-10-13 20:17:48 -0600

    Spawn manager spawns obstacles at random location at random time intervals.

--- Personal Game/Assets/Scripts/SpawnManager.cs
+++ Personal Game/Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs;
+    public int obstacleIndex;
+    public float xRange = 10f;
+    public float spawnPosZ = 20f;
+    public float spawnDelay = 3f;
+    public float spawnInterval;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("SpawnRandomObstacle", spawnDelay);
+    }
+
+    void SpawnRandomObstacle()
+    {
+        // Generate random obstacle index and random spawn location
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), 0, spawnPosZ);
+        spawnInterval = Random.Range(1f, 3f);
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+        Debug.Log(obstacleIndex);
+        Invoke("SpawnRandomObstacle", spawnInterval);
+
+        // Instantiate obstacle at random spawn location
+        Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+
     }
 
 

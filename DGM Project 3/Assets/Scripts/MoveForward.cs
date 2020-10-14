1dd70518713f736786fd1cbdbf619b80b5a66159
Hayden Kerr <10897906@my.uvu.edu>
2020-10-14 17:10:39 -0600

    Finished 3.1. Run and jump mechanics, obstacle movement, spawn manager created

--- DGM Project 3/Assets/Scripts/MoveForward.cs
+++ DGM Project 3/Assets/Scripts/MoveForward.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 15f;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.left * speed * Time.deltaTime);  
     }
 }
---
+++ DGM Project 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25,0,0);
+
+    private float spawnDelay;
+
+    private float repeatDelay;
+
+    // Invokes SpawnObstacles method at start of play
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", spawnDelay, repeatDelay);
+    }
+
+    // Spawns obstacles at the set spawn position
+    void SpawnObstacles()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}

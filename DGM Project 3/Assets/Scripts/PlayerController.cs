a719abb82dccb6e40d8faae82a7e4eb800a0f997
Hayden Kerr <10897906@my.uvu.edu>
2020-10-26 17:17:15 -0600

    Added particle effects for running and crashing, added music, jumping, and crashing sfx

--- DGM Project 3/Assets/Scripts/PlayerController.cs
+++ DGM Project 3/Assets/Scripts/PlayerController.cs
@@ -11,12 +11,22 @@ public class PlayerController : MonoBehaviour
     public bool gameOver;
     private Animator playerAnim;
 
+    //Particle effects
+    public ParticleSystem explostionParticle;
+    public ParticleSystem dirtParticle;
+
+    //Sound effects
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,9 +35,11 @@ public class PlayerController : MonoBehaviour
         //Makes it so player cannot double jump
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            isOnGround = false;
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
             playerAnim.SetTrigger("Jump_trig");
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
+            dirtParticle.Stop();
         }
     }
 
@@ -37,14 +49,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
-        //Creates Game Over text in log when collision with an obstacle occurs
+        //Stops the game
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
+            explostionParticle.Play();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             Debug.Log("Game Over");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
         }
     }
 }

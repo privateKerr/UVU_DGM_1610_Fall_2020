25b08f34ff03fe08a3d666e22f35808ec89a3364
Hayden Kerr <10897906@my.uvu.edu>
2020-12-05 17:21:46 -0700

    Created score keeping function in Prototype 5 and particle explosion when target is destroyed

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject>targets;
     private float spawnRate = 1;
+    public TextMeshProUGUI scoreText;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Starts coroutine to start spawning targets
         StartCoroutine(SpawnTarget());
+
+        //Sets score as 0 on screen at start of game
+        score = 0;
+        UpdateScore(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
+    //Spawns random targets at a set rate
     IEnumerator SpawnTarget()
     {
         while (true)
@@ -28,5 +30,12 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    //Updates score
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score : " + score;
     }
 }
--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -6,46 +6,52 @@ public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
     private float minSpeed = 12;
+    private GameManager gameManager;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
+        targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque());
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         transform.position = RandomSpawnPos();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
+    //Destroys target, updates score, and creates particle explosion on mouse click
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
+    //Destroys target when it passes through bottom barrier
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
     }
 
+    //Gives target random force upward when it spawns
     Vector3 RandomForce()
     {
         return Vector3.up * (Random.Range(minSpeed, maxSpeed));
     }
 
+    //Gives target a random torque value
     float RandomTorque()
     {
         return Random.Range(-maxTorque, maxTorque);
     }
 
+    //Spawns target in a random position
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);

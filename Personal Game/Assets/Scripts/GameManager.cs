1a1c1c0184a72d8a60d8ee0845c7397da537fc16
Hayden Kerr <10897906@my.uvu.edu>
2020-12-07 17:04:05 -0700

    Replaced spawnManager with GameManager

---
+++ Personal Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> obstacles;
+    public GameObject healthPrefab;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
+    private int score;
+    private int health;
+    private float spawnInterval;
+    public bool isGameActive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnObstacle());
+        UpdateScore(0);
+    }
+
+    IEnumerator SpawnObstacle()
+    {
+        while (true)
+        {
+            spawnInterval = Random.Range(1f, 2f);
+            yield return new WaitForSeconds(spawnInterval);
+            int index = Random.Range(0, obstacles.Count);
+            Instantiate(obstacles[index]);
+        }
+    }
+
+    void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+    }
+
+    void UpdateHealth(int healthTotal)
+    {
+        health += healthTotal;
+    }
+
+}
--- Personal Game/Assets/Scripts/Obstacle.cs
+++ Personal Game/Assets/Scripts/Obstacle.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-
     public float speed = 15f;
+    private float xRange = 10f;
+    private float spawnPosZ = 20f;
+    private GameManager gameManager;
+
+    void Start()
+    {
+        transform.position = SpawnRandomPosition();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
+
+
+    //Random spawn position
+    private Vector3 SpawnRandomPosition()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), 1, spawnPosZ);
+        return spawnPos;
+    }
+
+
 }
